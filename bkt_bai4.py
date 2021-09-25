41aa3d0c9ede3679c5d599ffca4bca85e3c99dfc
Mit202 <90501225+Mit202@users.noreply.github.com>
2021-09-25 16:00:28 +0700

    Add files via upload

---
+++ bkt_bai4.py
@@ -0,0 +1,10 @@
+n = int(input("Nhập số nguyên dương n = "));
+
+def tong(n):
+    t = 0;
+    while (n > 0):
+        t = t + n % 10;
+        n = int(n / 10);
+    return t;
+
+print("Tổng các chữ số của", n, "là", tong(n));
